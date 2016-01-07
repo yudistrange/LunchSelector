cfccefb86d850ce9dae68272bc6ea6ef5dd56866
udit <yudistrange@gmail.com>
2016-01-07 19:07:32 +0530

    First commit - New app created by leiningen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lunchselector
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lunchselector/core_test.clj
@@ -0,0 +1,7 @@
+(ns lunchselector.core-test
+  (:require [clojure.test :refer :all]
+            [lunchselector.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
